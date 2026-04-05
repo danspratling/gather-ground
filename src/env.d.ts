f79ddcb66797de3085aec862b41f8b6e8df9c81d
Dan Spratling <dan@skyward.digital>
2026-04-05 15:42:42 +0100

    fix(types): add ambient declarations for storybook-astro optional peer deps

---
+++ src/env.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="astro/client" />
+
+// Ambient declarations for optional peer dependencies used by
+// @storybook-astro/framework that we don't install (Preact, Solid, Svelte, Vue).
+// These stubs satisfy TypeScript without pulling in unused packages.
+declare module '@preact/preset-vite' {
+  export interface PreactPluginOptions {
+    include?: string | RegExp | Array<string | RegExp>;
+    exclude?: string | RegExp | Array<string | RegExp>;
+    [key: string]: unknown;
+  }
+}
+
+declare module 'vite-plugin-solid' {
+  export interface Options {
+    include?: string | RegExp | Array<string | RegExp>;
+    exclude?: string | RegExp | Array<string | RegExp>;
+    [key: string]: unknown;
+  }
+  export default function solid(options?: Options): unknown;
+}
+
+declare module '@sveltejs/vite-plugin-svelte' {
+  export interface PluginOptions {
+    [key: string]: unknown;
+  }
+  export interface SvelteConfig {
+    [key: string]: unknown;
+  }
+  export interface Options extends PluginOptions {}
+  export function svelte(options?: PluginOptions): unknown;
+}
+
+declare module '@vitejs/plugin-vue' {
+  export interface Options {
+    include?: string | RegExp | Array<string | RegExp>;
+    exclude?: string | RegExp | Array<string | RegExp>;
+    [key: string]: unknown;
+  }
+  export default function vue(options?: Options): unknown;
+}
+
+declare module '@vitejs/plugin-vue-jsx' {
+  export interface Options {
+    include?: string | RegExp | Array<string | RegExp>;
+    exclude?: string | RegExp | Array<string | RegExp>;
+    [key: string]: unknown;
+  }
+  export default function vueJsx(options?: Options): unknown;
+}
