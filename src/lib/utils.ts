b409d6146c1e2dfafad9cacd20eac0d426b0630c
Dan Spratling <dan@skyward.digital>
2026-04-07 19:11:43 +0100

    feat(tokens): add display-lg type token (48px/60px)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,7 +10,7 @@ import { extendTailwindMerge } from 'tailwind-merge';
 const twMerge = extendTailwindMerge({
   extend: {
     classGroups: {
-      'font-size': ['text-display-xl', 'text-display-md'],
+      'font-size': ['text-display-xl', 'text-display-lg', 'text-display-md'],
     },
   },
 });
